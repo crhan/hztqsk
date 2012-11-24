a0db06f5a344039a8181e97f386b370541ded7c3
@ruohanc <crhan123@gmail.com>
2012-11-24 12:24:02 +0800

    first blod

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hztqsk::Application.config.secret_token = '94b6a6edba824ceb64d0b7ab8b24ab5fd61f7aa6a56b39e46785e4ba4035022a404a0640e79f37c7d16b9ebf952106c7b6044f3a2ac0ff0a55d1b503f9a56ded'
