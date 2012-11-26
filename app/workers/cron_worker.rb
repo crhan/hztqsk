1cc83eaec76ed372cb44e543cfbc0cfdd75cb3f5
@ruohanc <crhan123@gmail.com>
2012-11-26 23:29:23 +0800

    可以收集气温, 气压和湿度了

--- app/workers/cron_worker.rb
+++ app/workers/cron_worker.rb
@@ -5,8 +5,14 @@ class CronWorker
   include Sidekiq::Worker
 
   def perform
-    data_datetime, attr = GetWeatherData.qxsk
-    data = WeatherData.find_or_create_by_data_datetime(data_datetime)
-    data.update_attributes(attr)
+    update_data GetWeatherData.get_temp
+    update_data GetWeatherData.get_humid
+    update_data GetWeatherData.get_press
+  end
+
+  private
+  def update_data arr
+    data_datetime, attr = arr
+    WeatherData.find_or_create_by_data_datetime(data_datetime).update_attributes(attr)
   end
 end
--- lib/get_weather_data.rb
+++ lib/get_weather_data.rb
@@ -2,38 +2,59 @@
 require 'net/http'
 
 class GetWeatherData
-  TEMPER_REG = /时间：(\d{1,2})时[#&\w&\d]*?温度：([-\d.]*?)℃(?:[#&\w&\d]*?最低温度：([-\d.]*?)℃)?(?:[#&\w&\d]*?最高温度：([-\d.]*?)℃)?/
-  PRESS_REG = /时间：(\d{1,2})时[#&\w&\d]*?气压：([.\d]*)(.*?)\'/
-  HUMID_REG = /时间：(\d{1,2})时[#&\w&\d]*?湿度：([.\d]*)/
-  VISIB_REG = /时间：(\d{1,2})时[#&\w&\d]*?能见度：([\d.]*)(.*?)\'/
+  TEMPER_REG = /时间：(\d{1,2})时温度：([-\d.]*?)℃(?:最低温度：([-\d.]*?)℃)?(?:最高温度：([-\d.]*?)℃)?/
+  PRESS_REG = /时间：(\d{1,2})时气压：([.\d]*)/
+  HUMID_REG = /时间：(\d{1,2})时湿度：([.\d]*)/
+  VISIB_REG = /时间：(\d{1,2})时能见度：([\d.]*)(.*?)\'/
 
   @@http ||= Net::HTTP.new("www.hzqx.com")
 
-  def self.temp
-    instance = self.new
-    resp = instance.get("/gzhfw/qxsk.asp")
-    body = resp.body.encode("utf-8", "gbk")
-    doc = Nokogiri::HTML(body)
-    result = doc.css("div#line_1").children.map {|e|
-      e.attributes["title"].value.gsub(/[\s ]/, '') rescue nil
-    }.reject {|i|
-      i.nil?
-    }.map {|j|
-      j.match(REGEX).captures
-    }.last
-    hour, temp, temp_low, temp_high = result.map{|e| e.nil? ? nil : e.to_f}
-    time = Time.now.to_a
-    time[0,3] = 0, 0, hour
-    time[4] -= 1 if hour > Time.now.hour
-    data_datetime = Time.local(*time).to_i
+  def self.get_temp
+    path="/gzhfw/qxsk.asp"
+    data = get_data(path, TEMPER_REG)
+    data_datetime, temp, temp_low, temp_high = data
     [ data_datetime, { :temp      => temp,
                        :temp_low  => temp_low,
                        :temp_high => temp_high } ]
   end
 
-  def self.pressure
-
+  def self.get_press
+    path="/gzhfw/qxsk-qy.asp"
+    data = get_data(path, PRESS_REG)
+    data_datetime, pressure = data
+    [ data_datetime, { :pressure => pressure } ]
   end
+
+  def self.get_humid
+    path = "/gzhfw/qxsk-sd.asp"
+    data = get_data(path, HUMID_REG)
+    data_datetime, humidity = data
+    [ data_datetime, { :humidity => humidity } ]
+  end
+
+
+  def self.get_data path, regexp
+    @@instance ||= self.new
+    doc = Nokogiri.HTML @@instance.get(path).body.encode("utf-8", "gbk")
+    hour_string, *attrs = html_parse(doc, regexp)
+    hour = hour_string.to_i
+    time = Time.now.to_a
+    time[0,3] = 0, 0, hour
+    time[4] -= 1 if hour > Time.now.hour
+    data_datetime = Time.local(*time).to_i
+
+    [ data_datetime, *attrs ]
+  end
+
+  def self.html_parse doc, regexp
+    doc.css("div#line_1").children.map {|e|
+      e.attributes["title"].value.gsub(/[\s ]/, '') rescue nil
+    }.reject {|i|
+      i.nil?
+    }.last
+    .match(regexp).captures
+  end
+
 
   def get(path)
     @@http.get path
