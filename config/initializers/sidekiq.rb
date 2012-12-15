f8d987762f3727a2f49be98ad9710cfdd9183a9e
@ruohanc <crhan123@gmail.com>
2012-12-15 23:35:41 +0800

    redis typo

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,8 @@
 Sidekiq.configure_server do |config|
-  config.redis = { :url => 'redis://localhost:6789/', :namespace => 'hzqxsk' }
+  config.redis = { :url => 'redis://localhost:6379/', :namespace => 'hzqxsk' }
 end
 
 # When in Unicorn, this block needs to go in unicorn's `after_fork` callback:
 Sidekiq.configure_client do |config|
-  config.redis = { :url => 'redis://localhost:6789/', :namespace => 'hzqxsk' }
+  config.redis = { :url => 'redis://localhost:6379/', :namespace => 'hzqxsk' }
 end
